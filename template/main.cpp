772b12b84ef07a5396e46757c0b57cc6c9d179fa
saxsir <saxsir.256@gmail.com>
2015-02-07 20:40:34 +0900

    [add] 問題を解き始める際のディレクトリテンプレートを用意
    cp -r template chapteX/hogehgoe で解き始められる

---
+++ template/main.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);i+=1)
+#define REP(i,n)  FOR(i,0,n)
+
+int n;
+void solve();
+
+int main() {
+  for (;;) {
+    // 入力
+    scanf("%d", &n);
+    if (n == 0) return 0;
+    REP(i, n) {
+      // scanf("%d", &a[i]);
+    }
+
+    solve();
+
+    // 出力
+    printf("%d\n", 0);
+  }
+}
+
+void solve() {
+}
