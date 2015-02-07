996c4bb1ce10d013a11dcc231e6fe30a571788af
saxsir <saxsir.256@gmail.com>
2015-02-07 19:00:15 +0900

    [wip] 問題の読み込み部分できた

--- chapter1/triangle/main.cpp
+++ chapter1/triangle/main.cpp
@@ -1,7 +1,19 @@
 #include <cstdio>
 using namespace std;
 
+int n;
+int a[100];
+
 int main() {
-  printf("Hello world!\n");
-  return 0;
+  for (;;) {
+    /*
+     * 問題の読み込み
+     */
+    scanf("%d", &n);
+    if (n == 0) return 0;
+
+    for (int i=0; i<n; i++) {
+      scanf("%d", &a[i]);
+    }
+  }
 }
