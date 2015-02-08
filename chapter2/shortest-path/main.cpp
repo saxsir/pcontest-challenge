e52f3479186650061e320cd8e1a673dd4bdc5fe3
saxsir <saxsir.256@gmail.com>
2015-02-08 16:12:25 +0900

    [add] bfs関数内を実装
    できた。

--- chapter2/shortest-path/main.cpp
+++ chapter2/shortest-path/main.cpp
@@ -13,6 +13,8 @@ const int INF = 100000000;
 int N, M;
 char field[100][100];
 int dist[100][100];
+int dx[4] = {1, 0, -1, 0},
+    dy[4] = {0, 1, 0, -1};
 
 int sx, sy;
 int gx, gy;
@@ -20,22 +22,30 @@ int gx, gy;
 queue<P> que;
 
 void bfs() {
-  // キューが空だったらreturn
-  if (que.size() <= 0) {
-    return;
+  for(;;) {
+    // キューが空だったらreturn
+    if (que.size() <= 0) return;
+
+    // キューの先頭を取り出す
+    P p = que.front();
+    que.pop();
+
+    // 取り出してきた状態がゴールならreturn
+    if (p.first == gx && p.second == gy) return;
+
+    // 取り出してきた状態から4方向に移動できるか調査
+    REP(i, 4) {
+      int nx = p.first + dx[i],
+          ny = p.second + dy[i];
+
+      // 移動できる かつ 今まで訪れたことがなければキューに追加してdistを取り出してきた状態+1で確定する
+      if (0 <= nx && nx < N && 0 <= ny && ny < M &&
+          field[nx][ny] != '#' && dist[nx][ny] == INF) {
+        dist[nx][ny] = dist[p.first][p.second] + 1;
+        que.push(P(nx, ny));
+      }
+    }
   }
-
-  // キューの先頭を取り出す
-  P p = que.front();
-  que.pop();
-
-  // 取り出してきた状態がゴールならreturn
-  if (p.first == gx && p.second == gy) {
-    return;
-  }
-
-  // 取り出してきた状態から4方向に移動できるか調査
-  // 移動できる かつ 今まで訪れたことがなければキューに追加してdistを取り出してきた状態+1で確定する
 }
 
 int solve() {
@@ -58,7 +68,7 @@ int solve() {
 
   // Sの座標を0, キューの先頭に追加して幅優先探索スタート
   dist[sx][sy] = 0;
-  que.push(P(sy, sy));
+  que.push(P(sx, sy));
   bfs();
 
   return dist[gx][gy];
