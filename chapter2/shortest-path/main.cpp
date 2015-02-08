50132f73852f7e841471a37aa873d124a38317b9
saxsir <saxsir.256@gmail.com>
2015-02-08 13:52:07 +0900

    [modify] 解き方のコメントを追加
    wip commit

--- chapter2/shortest-path/main.cpp
+++ chapter2/shortest-path/main.cpp
@@ -5,11 +5,19 @@ using namespace std;
 #define FOR(i,a,b) for(int i=(a);i<(b);i+=1)
 #define REP(i,n)  FOR(i,0,n)
 
+const int INF = 100000000;
+
 int N, M;
 char field[100][100];
+char dist[100][100];
 
+// 同じ大きさのフィールド作成
+// 全部 INF で初期化
+// S の座標を0にして、1ターンで行ける距離は1, 2ターンで行ける距離は2, ...みたいに数値を入れていく
+// G に辿り着いたら終わり
 int solve() {
-  return 22;
+  int ans = 0;
+  return ans;
 }
 
 int main() {
