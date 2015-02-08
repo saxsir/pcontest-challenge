027f2ccc32812eb9b8c694bd4bb9284ad80f4c00
saxsir <saxsir.256@gmail.com>
2015-02-08 15:47:36 +0900

    [add] 幅優先探索の流れを書いた

--- chapter2/shortest-path/main.cpp
+++ chapter2/shortest-path/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+typedef pair<int, int> P;
+
 #define FOR(i,a,b) for(int i=(a);i<(b);i+=1)
 #define REP(i,n)  FOR(i,0,n)
 
@@ -12,13 +14,54 @@ int N, M;
 char field[100][100];
 char dist[100][100];
 
-// 同じ大きさのフィールド作成
-// 全部 INF で初期化
-// S の座標を0にして、1ターンで行ける距離は1, 2ターンで行ける距離は2, ...みたいに数値を入れていく
-// G に辿り着いたら終わり
+int sx, sy;
+int gx, gy;
+
+queue<P> que;
+
+void bfs() {
+  // キューが空だったらreturn
+  if (que.size() <= 0) {
+    return;
+  }
+
+  // キューの先頭を取り出す
+  P p = que.front();
+  que.pop();
+
+  // 取り出してきた状態がゴールならreturn
+  if (p.first == gx && p.second == gy) {
+    return;
+  }
+
+  // 取り出してきた状態から4方向に移動できるか調査
+  // 移動できる かつ 今まで訪れたことがなければキューに追加してdistを取り出してきた状態+1で確定する
+}
+
 int solve() {
-  int ans = 0;
-  return ans;
+  // 全ての点を INF で初期化
+  REP(i, N) {
+    REP(j, M) {
+      dist[i][j] = INF;
+      // ついでにスタート地点の座標を取得
+      if (field[i][j] == 'S') {
+        sx = i;
+        sy = j;
+      }
+      // ついでにゴール地点の座標を取得
+      if (field[i][j] == 'G') {
+        gx = i;
+        gy = j;
+      }
+    }
+  }
+
+  // Sの座標を0, キューの先頭に追加して幅優先探索スタート
+  dist[sx][sy] = 0;
+  que.push(P(sy, sy));
+  bfs();
+
+  return dist[gx][gy];
 }
 
 int main() {
