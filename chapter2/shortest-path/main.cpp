4363e0b43b2a0467591f760a6d30dd962aed25db
saxsir <saxsir.256@gmail.com>
2015-02-08 13:40:29 +0900

    [add] 問題の読み込み処理
    refs #1 problem.txtから問題を読み込む処理できた

--- chapter2/shortest-path/main.cpp
+++ chapter2/shortest-path/main.cpp
@@ -5,10 +5,26 @@ using namespace std;
 #define FOR(i,a,b) for(int i=(a);i<(b);i+=1)
 #define REP(i,n)  FOR(i,0,n)
 
+int N, M;
+char field[100][100];
+
 void solve() {
 }
 
 int main() {
+  char dummy;
+
+  // 入力
+  scanf("%d%d", &N, &M);
+  if (N == 0) return 0;
+
+  // 改行コードを読み飛ばす
+  scanf("%c", &dummy);
+
+  REP(i, N) {
+    REP(j, M) {
+      scanf("%c", &field[i][j]);
+    }
+    scanf("%c", &dummy);
+  }
 }
-
-
