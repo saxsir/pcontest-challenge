8dac5402f8e7a71f1db93849f6a3c5b349c3f5e8
saxsir <saxsir.256@gmail.com>
2015-02-08 13:23:21 +0900

    [init] init problem and answer
    refs #1 問題を解き始めるために初期化

---
+++ chapter2/shortest-path/main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);i+=1)
+#define REP(i,n)  FOR(i,0,n)
+
+void solve() {
+}
+
+int main() {
+}
+
+
